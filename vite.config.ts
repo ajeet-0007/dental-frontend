355a3e0a6517804197b895c373d7fe8ba18e53a2
Apple <apple@Apples-MacBook-Air.local>
2026-07-30 00:02:23 +0530

    enable PWA devOptions for local development

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
       includeAssets: ['DentZoo_Logo.svg'],
       manifest: {
         name: 'Dentzoo - Dental Products Online',
